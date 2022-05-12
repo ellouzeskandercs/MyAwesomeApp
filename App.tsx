bba7495eeb48e90bfb3e8257797fcf0b4fec61cc
Skander Ellouze <skander.ellouze@student-cs.fr>
2022-05-12 16:18:09 +0200

    Remove test component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import HelloComponent from './src/HelloComponent';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.App}>
-      <HelloComponent />
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={styles.App} />;
 };
 
 const styles = StyleSheet.create({
-  App: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  App: {},
 });
 
 export default App;
